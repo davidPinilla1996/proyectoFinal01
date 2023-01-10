ece8d3da8764d6518ea7cacff93d748b27659c0c
AndresfelipeHERO <66238252+andresfelipe3112@users.noreply.github.com>
2023-01-10 14:11:53 -0500

    agregamos el proyecto final

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+      <TextInput style={styles.input} placeholder="hola mundo wrold" />
+      <Button title="Add" color="#145C9E"/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  inputContainer: {
+    margin: 50,
+    marginHorizontal: 20,
+  },
+  input: {
+   borderBottomColor: "#145C9E",
+   borderBottomWidth: 1,
+   height: 40,
+   color: "#161E22"
+  }
+});
