ed6fca562fc085adea1b3565529050296f45794d
new name <davidpinillame1995@gmail.com>
2023-01-12 15:51:16 -0500

    proyecto actualizado

--- App.js
+++ App.js
@@ -1,59 +1,91 @@
-//importando el useState desde react
-import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Modal, TouchableOpacity, Systrace } from 'react-native';
 
 export default function App() {
-  //inicializando el estado importado desde react
-const [task, setTask] = useState("");
-const[tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
 
-//creo un metodo que reciba el texto que contiene el campo 
-const onHandlerChange = (text) => {
-  //setienado el valor se asignacion como el de mi estado (setTask)
-  setTask(text)
-}
+  const onHandlerChange = (text) => {
+    setTask(text)
+  }
 
-const onHandlerSubmit = () => {
-  setTasks([
-    ...task,
-    {
-      id: Math.random().toString(),
-      value: task
-    }
-  ]);
-  setTask("");
-}
+  const onHandlerSubmit = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random().toString(),
+        value: task
+      }
+    ]);
+    setTask('');
+  }
 
-console.warn("task",task)
+  const onHandlerModal = (item) => {
+    setIsModalVisible(!isModalVisible)
+    setSelectedTask(item);
+  }
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerModal(item)}>
+      <Text style={styles.itemList}>{item.value}</Text>
+    </TouchableOpacity>
+  )
+
+  const keyExtractor = (item) => item.id;
+  
+  const onHandleCancel = () => {
+    setIsModalVisible(!isModalVisible);
+    setSelectedTask(null);
+  }
+
+  const onHandleDelete = () => {
+    setTasks((prevTaskList) => prevTaskList.filter((task) => task.id !== selectedTask.id));
+    setIsModalVisible(!isModalVisible);
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-      <TextInput 
-      style={styles.input} 
-      autoComplete="off"
-      autoCorrect={false}
-      autoCapitalize="none"
-      placeholder="hola mundo wrold" 
-      value={task}
-      //El onChangeText me retorna el valor que yo e escrito dentro del campo
-      onChangeText={onHandlerChange}
-      />
-      <Button 
-      disabled={!task}
-      title="Add" 
-      color="#145C9E"
-      //el evento de onPress se ejecuta cada ves que se le da click en el boton en este caso
-      onPress={onHandlerSubmit}
-      />
-      <View>
-        {
-          tasks.map((item) => (
-             <Text key={item.id}>{item.vale}</Text>
-          ))
-        }
+        <TextInput 
+          style={styles.input} 
+          placeholder='add a new task' 
+          autoComplete='off'
+          autoCorrect={false}
+          autoCapitalize='none'
+          value={task}
+          onChangeText={onHandlerChange}
+        />
+        <Button disabled={!task} title='Add' color='#626893' onPress={onHandlerSubmit} />
       </View>
-      </View>
+      <FlatList 
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.listContainer}
+      />
+      <Modal visible={isModalVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Task Detail</Text>
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>Are you sure to delete this item?</Text>
+            <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button 
+              title='Cancel'
+              color='#626893'
+              onPress={onHandleCancel}
+            />
+            <Button
+              title='Delete'
+              color='red'
+              onPress={onHandleDelete}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -64,16 +96,67 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    margin: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
     marginHorizontal: 20,
   },
   input: {
-   width: "75%",
-   borderBottomColor: "#145C9E",
-   borderBottomWidth: 1,
-   height: 40,
-   color: "#161E22"
+    width: '75%',
+    borderBottomColor: '#626893',
+    borderBottomWidth: 1,
+    height: 40,
+    color: '#212121'
+  },
+  listContainer: {
+    marginHorizontal: 20,
+    marginTop: 40
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#626893',
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  itemList: {
+    fontSize: 14,
+    color: '#ffffff',
+    fontWeight: 'bold'
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalDetailContainer: {
+    padddingVertical: 20,
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: '#212121'
+  },
+  selectedTask: {
+    fontSize: 14,
+    color: '#212121',
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  modalButtonContainer: {
+    width: '70%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
   }
 });
