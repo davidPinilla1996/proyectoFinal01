d68c9dd01ec6f58783a2c2881ffe256ee4dda169
new name <davidpinillame1995@gmail.com>
2023-01-14 10:46:10 -0500

    proyecto actializado

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
+//importamos el useState desde react 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList, Modal, TouchableOpacity, Systrace } from 'react-native';
+//importamos varios componentes desde react
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Modal, TouchableOpacity} from 'react-native';
+
+
 
 export default function App() {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
+  //inicializamos el useState.
+  //Es una constante que tiene un array y dicha array contiene el primer elemento que seria el valor del estado y el segundo elemento que seria la funcion que se ocupa de actualizar el estado, lo igualamos a useState y podemos inicializar el estado con un valor.
+  //un estado puede ser datos primarios, un objeto, una funcion, otro componente etc...  
+                         //inicializamos el useState como un estado vacio por que el campo del formualrio esta vacio inicialmente.
+  const [tarea, setTask] = useState('');
+  const [tareas, setTasks] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
 
+  //onHandlerChange recibe el texto que yo escriba dentro del campo.
   const onHandlerChange = (text) => {
+    //setiando el valor como asignacion de mi estado.
     setTask(text)
   }
 
   const onHandlerSubmit = () => {
     setTasks([
-      ...tasks,
+      ...tareas,
       {
         id: Math.random().toString(),
-        value: task
+        value: tarea
       }
     ]);
     setTask('');
@@ -28,6 +38,7 @@ export default function App() {
   }
 
   const renderItem = ({ item }) => (
+    //                                             El onPress Controla que se llamará(que pasara) cuando el usuario toque el botón.
     <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerModal(item)}>
       <Text style={styles.itemList}>{item.value}</Text>
     </TouchableOpacity>
@@ -54,13 +65,18 @@ export default function App() {
           autoComplete='off'
           autoCorrect={false}
           autoCapitalize='none'
-          value={task}
+          //el valor de useState que ahora conocemos como tarea lo vamos a asignar como valor por defecto del campo.
+          //soncronisamos el useState tarea con el valor del campo por que eso nos va a permitir manejar el valor que esta en el campo,
+          value={tarea}
+          // el TextInput o los input tienen un metodo o un evento que se ejecuta al momento de nosotros modificar el campo que seria el onChange.
+          // tenemos la posibilidad de acceder directamente al texto con el metodo de onChangeText.
+          // el onChangeText me retorna el valor que yo e escrito dentro del campo.
           onChangeText={onHandlerChange}
         />
-        <Button disabled={!task} title='Add' color='#626893' onPress={onHandlerSubmit} />
+        <Button disabled={!tarea} title='Add' color='#626893' onPress={onHandlerSubmit} />
       </View>
       <FlatList 
-        data={tasks}
+        data={tareas}
         renderItem={renderItem}
         keyExtractor={keyExtractor}
         style={styles.listContainer}
