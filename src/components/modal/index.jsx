261d899a68d94c1030c790a3954ef7cb6bc8e09a
new name <davidpinillame1995@gmail.com>
2023-01-18 10:48:47 -0500

    proyecto actualizado

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import { Modal, View, Text, Button } from 'react-native'
 import { styles } from "./styles";
 
-const CustomModal = ({ isModalVisible, selectedTask, onHandleCancel, onHandleDelete}) => {
+const CustomModal = ({ isModalVisible, selectedTask, onHandleCancel, onHandleDelete,onHandleTacha}) => {
     return (
         <Modal visible={isModalVisible} animationType='slide'>
         <View style={styles.modalContainer}>
-          <Text style={styles.modalTitle}>Task Detail</Text>
+          <Text style={styles.modalTitle}>Tarea detallada</Text>
           <View style={styles.modalDetailContainer}>
-            <Text style={styles.modalDetailMessage}>Are you sure to delete this item?</Text>
             <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+            <Text style={styles.modalDetailMessage}>Que deseas hacer?</Text>
           </View>
           <View style={styles.modalButtonContainer}>
             <Button 
-              title='Cancel'
+              title='Cancelar'
               color='#626893'
               onPress={onHandleCancel}
             />
             <Button
-              title='Delete'
+              title='Borrar'
               color='red'
               onPress={onHandleDelete}
             />
